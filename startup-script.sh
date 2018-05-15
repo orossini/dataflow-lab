b77bc913cfcbdd4534fd001b747ca25940b1002c
Olivier Rossini <orossini@google.com>
2018-05-15 16:30:41 -0400

    updated startup-script.sh

--- startup-script.sh
+++ startup-script.sh
@@ -2,4 +2,9 @@
 # startup script for dataflow lab
 
 gcloud pubsub topics create devicesdata
+echo "pubsub topic created"
+gsutil mb -c regional -l europe-west1 gs://$PROJECTID/imagesin
+gsutil mb -c regional -l europe-west1 gs://$PROJECTID/imagesout
+echo "buckets created"
+return 0
 
