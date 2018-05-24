00015682e650f71aaa78623b90ee854115c63951
Olivier Rossini <orossini@google.com>
2018-05-24 18:14:34 -0400

    update startup-script

--- startup-script.sh
+++ startup-script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #test startup-script for vm-dev in qwiklab DM script
 sudo apt-get update
-sudo apt-get install -y git
 sudo apt-get install -y openjdk-8-jdk
 # java will be installed in /usr/lib/jvm/java-8-openjdk-amd64/
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
@@ -24,6 +23,6 @@ echo "export BUCKET_OUT_PATH=gs://$PROJECTID-imagesout" >> ~/.profile
 gsutil mb -c regional -l us-west1 gs://$PROJECTID-dataflowstagging
 gcloud iot registries create $PROJECTID-iotregistry --project=$PROJECTID --region=us-central1 --event-notification-config=topic=$TOPIC_URI
 gcloud iam service-accounts create sa-iotdevice --display-name "sa-iotdevice"
-gcloud projects add-iam-policy-binding $PROJECTID --member serviceAccount:sa-iotdevice@£PROJECTID.iam.gserviceaccount.com --role roles/storage.objectCreator
-gcloud iam service-accounts keys create ~/key.json --iam-account sa-iotdevice@PROJECT-I$PROJECTIDD.iam.gserviceaccount.com
+gcloud projects add-iam-policy-binding $PROJECTID --member serviceAccount:sa-iotdevice@$PROJECTID.iam.gserviceaccount.com --role roles/storage.objectCreator
+gcloud iam service-accounts keys create ~/key.json --iam-account sa-iotdevice@$PROJECTIDD.iam.gserviceaccount.com
  
