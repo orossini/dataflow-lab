718d4edc54c4714440d48173fb3e8ca6b5a3cf1e
Olivier Rossini <orossini@google.com>
2018-05-26 16:15:39 +0200

    update startup script

--- startup-script.sh
+++ startup-script.sh
@@ -26,4 +26,6 @@ gcloud iot registries create $PROJECTID-iotregistry --project=$PROJECTID --regio
 gcloud iam service-accounts create sa-iotdevice --display-name "sa-iotdevice"
 gcloud projects add-iam-policy-binding $PROJECTID --member serviceAccount:sa-iotdevice@$PROJECTID.iam.gserviceaccount.com --role roles/storage.objectCreator
 gcloud iam service-accounts keys create ~/sa-key.p12 --key-file-type="p12" --iam-account=sa-iotdevice@$PROJECTID.iam.gserviceaccount.com
+gsutil cp ~/sa-key.p12 gs://$PROJECTID-temp/sa-key.p12
+return 0
  
