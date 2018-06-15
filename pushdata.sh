8e1b1b72f6b09a5d29fd829ca55c0811865db832
Olivier Rossini <orossini@google.com>
2018-06-15 17:01:13 +0200

    updated push script

--- pushdata.sh
+++ pushdata.sh
@@ -2,7 +2,7 @@
 # simple cmd to push data (image file) to GCS bucket 
 # and to publish a pubsub message
 
-gsutil cp ~/bootcamp-ml-next2018/sampledata/candy1.jpg gs://$BUCKET_IN/candy1.jpg
+gsutil cp ~/dataflow-lab/sampledata/candy1.jpg gs://$BUCKET_IN/candy1.jpg
 echo data copied to GCS bucket $BUCKET_IN
 gcloud beta pubsub topics publish $PUBSUB_TOPIC --message "candy1.jpg,almondjoy"
 echo msg published to Pubsub topic $PUBSUB_TOPIC
