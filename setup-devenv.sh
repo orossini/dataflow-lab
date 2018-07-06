15249e75457288e873e7355f6e8248635b0e1c54
Olivier Rossini <orossini@google.com>
2018-07-06 08:36:41 +0000

    updated bash scrip setup-devenv

--- setup-devenv.sh
+++ setup-devenv.sh
@@ -10,11 +10,13 @@ sudo mkdir -p /home/devtools/
 cd /home/devtools
 sudo -p /home/devtools/ wget http://www-us.apache.org/dist/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
 sudo tar xzvf /home/devtools/apache-maven-3.5.3-bin.tar.gz
+cp -R ~/dataflow-lab/.m2 ~/
 echo 'export PATH=$PATH:/home/devtools/apache-maven-3.5.3/bin' | sudo tee -a /etc/bash.bashrc
 gcloud pubsub topics create iotdata
 echo 'export TOPIC_URI=projects/$PROJECTID/topics/iotdata' | sudo tee -a /etc/bash.bashrc
 gsutil mb -c regional -l us-west1 gs://$PROJECTID-imagesin
 echo 'export BUCKET_IN_PATH=gs://$PROJECTID-imagesin' | sudo tee -a /etc/bash.bashrc
+export BUCKET_IN_PATH=gs://$PROJECTID-imagesin
 gsutil mb -c regional -l us-west1 gs://$PROJECTID-imagesout
 echo 'export BUCKET_OUT_PATH=gs://$PROJECTID-imagesout' | sudo tee -a /etc/bash.bashrc
 gsutil mb -c regional -l us-west1 gs://$PROJECTID-dataflowstagging
@@ -24,4 +26,5 @@ gcloud iam service-accounts create sa-iotdevice --display-name 'sa-iotdevice'
 gcloud projects add-iam-policy-binding $PROJECTID --member serviceAccount:sa-iotdevice@$PROJECTID.iam.gserviceaccount.com --role roles/storage.objectCreator
 gcloud iam service-accounts keys create ~/sa-key.p12 --key-file-type='p12' --iam-account=sa-iotdevice@$PROJECTID.iam.gserviceaccount.com
 gsutil cp ~/sa-key.p12 gs://$PROJECTID-temp/sa-key.p12
+source /etc/bash.bashrc
 
