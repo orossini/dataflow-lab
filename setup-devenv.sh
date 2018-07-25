8e900fd75ca2b2652946229baed1430be000fcf8
Olivier Rossini <orossini@users.noreply.github.com>
2018-07-25 11:29:34 -0700

    update setup-devenv.sh
    to have same iot registry name accross labs

--- setup-devenv.sh
+++ setup-devenv.sh
@@ -22,7 +22,7 @@ gsutil mb -c regional -l us-west1 gs://$PROJECTID-imagesout
 echo 'export BUCKET_OUT_PATH=gs://$PROJECTID-imagesout' | sudo tee -a /etc/bash.bashrc
 gsutil mb -c regional -l us-west1 gs://$PROJECTID-dataflowstagging
 gsutil mb -c regional -l us-west1 gs://$PROJECTID-temp
-gcloud iot registries create $PROJECTID-iotregistry --project=$PROJECTID --region=us-central1 --event-notification-config=topic=$TOPIC_URI
+gcloud iot registries create iotregistry --project=$PROJECTID --region=us-central1 --event-notification-config=topic=$TOPIC_URI
 gcloud iam service-accounts create sa-iotdevice --display-name 'sa-iotdevice'
 gcloud projects add-iam-policy-binding $PROJECTID --member serviceAccount:sa-iotdevice@$PROJECTID.iam.gserviceaccount.com --role roles/storage.objectCreator
 gcloud iam service-accounts keys create ~/sa-key.p12 --key-file-type='p12' --iam-account=sa-iotdevice@$PROJECTID.iam.gserviceaccount.com
